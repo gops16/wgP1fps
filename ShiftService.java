7cda53ee1869c33d608f999ac3dfddb01c21af6b
gops16 <33510995+gops16@users.noreply.github.com>
2018-02-16 10:02:44 +0800

    Update ShiftService.java

--- ShiftService.java
+++ ShiftService.java
@@ -1,18 +1,2 @@
-/**
- * 
- */
-package fte.rascan.service;
-
-import java.util.UUID;
-
-import org.springframework.stereotype.Service;
-
-import fte.api.UniversalCRUD;
-import fte.rascan.entity.Shifts;
-
-/**
- * @author genzzz
- *
- */
 @Service
 public interface ShiftService extends UniversalCRUD<Shifts, UUID> {}
