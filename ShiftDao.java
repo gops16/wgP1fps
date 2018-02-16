6b39539ec42508a304859d3ca3d0efb9b04660e1
gops16 <33510995+gops16@users.noreply.github.com>
2018-02-16 10:02:04 +0800

    Update ShiftDao.java

--- ShiftDao.java
+++ ShiftDao.java
@@ -1,15 +1,1 @@
-/**
- * 
- */
-package fte.rascan.dao;
-
-import java.util.UUID;
-
-import fte.api.UniversalCRUD;
-import fte.rascan.entity.Shifts;
-
-/**
- * @author genzzz
- *
- */
 public interface ShiftDao extends UniversalCRUD<Shifts, UUID> {}
